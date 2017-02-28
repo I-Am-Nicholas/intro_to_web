53fe8609b20d687f095cb9a790067225d7a2a6cd
Nicholas Leacock <nicholas@swissmail.com>
2017-02-28 12:59:46 +0000

    WIP Post ed Params

--- app.rb
+++ app.rb
@@ -1,20 +1,7 @@
 require 'sinatra'
 
 
-get '/' do
-  "I'm riding shotgun. I've got your back. Sleep easy."
-end
-
-
-get '/random-cat' do
-
-  @names = ["T'Challa", "Steve Rogers", "Wanda Maximoff"].sample
-
-  erb(:index)
-
-end
-
-get '/named-cat' do
+post '/named-cat' do
 
   p params
 
@@ -22,5 +9,14 @@ get '/named-cat' do
   @last_name = params[:last_name]
 
   erb(:index)
+
+end
+
+
+get '/form' do
+
+
+  erb(:index2)
+
 
 end
